9abbeade8d52fb967e79d21ae07273c38c7fc87a
mhkmcp <mhkmcp@yahoo.com>
2021-02-27 13:54:27 +0600

    02: Added Routing

--- index.js
+++ index.js
@@ -3,8 +3,20 @@ var express = require('express');
 app = express();
 
 app.get('/', function (req, res) {
-    res.send("Hello Home!");
+    res.send("Home Page!");
 })
+
+app.put('/about', function (req, res) {
+    res.send("About Page!");
+})
+
+app.post('/contact', function (req, res) {
+    res.send("Contact Page!");
+})
+app.put('/terms', function (req, res) {
+    res.send("Terms Page!");
+})
+
 
 app.listen(9000, function () {
     console.log("Server Run Success");
